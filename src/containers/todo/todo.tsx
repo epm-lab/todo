6f9d5394e7b7804f20e73ddc9520f6519e8acc60
Marina Sevkovich <slolkunchik@gmail.com>
2020-07-08 19:37:02 +0300

    feat: add todo container

--- src/containers/todo/todo.tsx
+++ src/containers/todo/todo.tsx
@@ -1,9 +1,49 @@
-import React, { Component } from "react";
+import React, { ReactElement } from "react"
+import {useDispatch, useSelector  } from 'react-redux'
+import { InjectedFormProps, reduxForm, Field } from 'redux-form'
+import { RootState, Task } from '../../store/appState'
+import { addTask } from '../../store/actions'
+import { AppHeader } from '../../components/app-header/appHeader'
+import 'antd/dist/antd.css'
+import './todo.css'
 
-import "./todo.css";
+function ToDo(props: InjectedFormProps): ReactElement{
+  const dispatch = useDispatch()
+  const filter = useSelector((state: RootState) => state.filter)
+  const tasks = useSelector((state: RootState) => state.tasks)
+  const isTasksExist = tasks && tasks.length > 0
+  const { handleSubmit } = props
 
-export class ToDo extends Component {
-  render() {
-    return <p>todo list here!</p>;
+  const handleAddTask = (values: any) => {
+    const taskText: string = values.toDoInput
+    if (taskText && taskText.length > 0) {
+      const newTask: Task = {
+        id: (new Date()).getTime(),
+        text: taskText,
+        isCompleted: false,
+      }
+      dispatch(addTask(newTask))
+    }
   }
+
+  return (
+    <form className="form" onSubmit={handleSubmit(handleAddTask)}>
+      {/*TODO: import AppHeader component here instead of App*/}
+      {/*<AppHeader amount={tasks.length} activeFilter={filter} />*/}
+      <Field
+        className="main-input"
+        name="toDoInput"
+        component="input"
+        type="text"
+        placeholder="Enter you task here..."
+      />
+      {/*TODO: import TodoList component*/}
+      {/*{isTasksExist && <TodoList tasksList={tasks} />}*/}
+    </form>
+  )
 }
+
+export default reduxForm({
+  form: 'toDo',
+})(ToDo);
+
--- src/store/appState.ts
+++ src/store/appState.ts
@@ -1,11 +1,18 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
+import { InjectedFormProps } from 'redux-form';
 
 export interface Task {
   id: number,
   text: string,
   isCompleted: boolean,
+}
+
+export interface RootState {
+  tasks: Task[],
+  filter: string,
+  form: InjectedFormProps,
 }
 
 const store = createStore(rootReducer, composeWithDevTools());
