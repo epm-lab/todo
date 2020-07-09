ab9d6b28210ebbb7f4d759eff7c52555fa6b01a8
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2020-07-09 18:46:28 +0300

    feat: implement function delete into TodoListItem

--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -4,13 +4,15 @@ import TodoListItem from '../todo-list-item/todo-list-item';
 
 import './todo-list.css';
 
-const TodoList = ({ tasksList }:TodoTypes) => (
-  <ul className="todo-list">
+const TodoList = ({ tasksList }:TodoTypes) => {
+  return (
+    <ul className="todo-list">
     {tasksList.map(({ id, text, isCompleted }) => (
       <TodoListItem text={text} isCompleted={isCompleted} key={id} id={id}/>
     ))}
   </ul>
-);
+  );
+}
 
 interface ItemTypes {
     id: number,
