f62d6d5a91d9e8a8d11a9a10b35a38eb8b8a2ceb
Marina Sevkovich <slolkunchik@gmail.com>
2020-07-10 22:23:16 +0300

    feat: add edit task function into modal window

---
+++ src/containers/modal-window/modal-window.tsx
@@ -0,0 +1,63 @@
+import { Modal, Button, Input } from 'antd';
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeTask } from '../../store/actions';
+
+interface ModalType {
+	text: string;
+	id: number;
+}
+ 
+const ModalWindow = ({ text, id }: ModalType ) => {
+	const dispatch = useDispatch()
+  const [ visible, setVisible ] = useState(false);
+  const [ value, setInputValue ] = useState(text);
+
+  const showModal = ():void => {
+    setVisible(true);
+  };
+
+	const handleOk = ():void => {
+		setVisible(false);
+		if (text !== value) {
+			dispatch(changeTask(id, value))
+		}
+	};
+
+	const handleCancel = ():void => {
+		setVisible(false)
+		setInputValue(text)
+	};
+
+	const handleEnter = (e: React.KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			handleOk()
+		}
+	}
+
+	return (
+			<div>
+			<Button type="primary" onClick={showModal}>
+					Editing
+			</Button>
+			<Modal
+					title="Editing"
+					visible={visible}
+					onOk={handleOk}
+					onCancel={handleCancel}
+			>
+			<Input
+				type="text"
+				defaultValue={text}
+				onChange={(e: React.FormEvent<HTMLInputElement>) => {
+					setInputValue(e.currentTarget.value)
+				}}
+				onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => handleEnter(e)}
+			/>
+			</Modal>
+			</div>
+	);
+}
+
+export default ModalWindow
